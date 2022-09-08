6070ace93ccee33695917b26f7ff35bbec6ae63c
Lawrence Enang <Lawrence.Enang@fcmb.com>
2022-09-08 16:30:54 +0100

    changesOnDashboard

--- HahnApplicationClients/src/app/master/auth/lazyloader.routes.ts
+++ HahnApplicationClients/src/app/master/auth/lazyloader.routes.ts
@@ -1,19 +1,18 @@
 
 import { Routes } from '@angular/router';
 import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
+import { PageNotFoundComponent } from 'src/app/page-not-found/page-not-found.component';
 import { AuthComponent } from './auth.component';
 
 export const appRoutes: Routes = [
   {
-    path: '',
-    component: AuthComponent,
+   path: '',  component: AuthComponent,
     children: [
-      { path: 'dashboard-input', component: DashboardComponent},
-     
-      { path: 'candidate', loadChildren: () => import('../candidate/candidate.module').then(a => a.CandidateModule) },
+     {path:'dashboard', component:DashboardComponent},
+     { path: 'candidate', loadChildren: () => import('../candidate/candidate.module').then(a => a.CandidateModule) },
       { path: 'global', loadChildren: () => import('../candidateType/candidate-type.module').then(a => a.CandidateTypeModule) },
       
-      { path: '**', redirectTo: 'dashboard-input' },
+      { path: '**', redirectTo: 'not-found' }
     ],
   },
 ];
